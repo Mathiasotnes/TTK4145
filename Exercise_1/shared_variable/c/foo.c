62dfdadde86e2b70a90467ca875fbb9048925917
mathias.otnes@gmail.com <mathias.otnes@gmail.com>
2024-01-18 19:35:28 +0100

    Exercise 2

--- Exercise_1/shared_variable/c/foo.c
+++ Exercise_1/shared_variable/c/foo.c
@@ -5,27 +5,42 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t lock;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    // TODO: increment i 1_000_000 times
+    for(int j = 0; j < 1000000; j++) {
+        pthread_mutex_lock(&lock);
+        i += 1;
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
+    for(int j = 0; j < 999900; j++) {
+        pthread_mutex_lock(&lock);
+        i -= 1;
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
 
 int main(){
-    // TODO: 
-    // start the two functions as their own threads using `pthread_create`
-    // Hint: search the web! Maybe try "pthread_create example"?
-    
-    // TODO:
-    // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+
+    // Spawn threads
+    pthread_t thread1;
+    pthread_t thread2;
+    pthread_create(&thread1, NULL, incrementingThreadFunction, "Thread 1");
+    pthread_create(&thread2, NULL, decrementingThreadFunction, "Thread 2");
+
+    // Initialize mutex
+    pthread_mutex_init(&lock, NULL);
+
+    // Wait for threads to finish
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
     
     printf("The magic number is: %d\n", i);
     return 0;
--- Exercise_2/network/src/main.rs
+++ Exercise_2/network/src/main.rs
@@ -1,17 +1,22 @@
-use std::net::{UdpSocket, TcpListener};
+use std::net::{UdpSocket, TcpListener, TcpStream};
 use std::io::{Result, Read, Write};
 
 fn main() -> Result<()> {
-    tcp_test()
+    udp_send();
+    udp_recv()
+    // tcp_connect()
 }
 
-fn udp_test() -> Result<()> {
+fn udp_recv() -> Result<()> {
 
     /* Create socket */
-    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    let socket = UdpSocket::bind("0.0.0.0:20022")?;
+
+    /* Send packet */
+    // socket.send_to(b"Hej hej", "10.100.23.1:30000")?;
 
     /* Receive data */
-    let mut buf = [0; 20];
+    let mut buf = [0; 50];
     let (amt, src) = socket.recv_from(&mut buf)?;
 
     /* Print data */
@@ -19,12 +24,16 @@ fn udp_test() -> Result<()> {
     println!("Packet received from: {}", src.to_string());
     println!("Received data: {}", received_data);
 
-    /* Reverse packet */
-    let reversed_data = &mut buf[..amt];
-    reversed_data.reverse();
-    socket.send_to(reversed_data, &src)?;
-    println!("Packet sent back to: {}", src.to_string());
-    println!("Sent data: {}", std::str::from_utf8(&reversed_data).expect("Could not write buffer as string"));
+    Ok(())
+}
+
+fn udp_send() -> Result<()> {
+
+    /* Create socket */
+    let socket = UdpSocket::bind("0.0.0.0:20022")?;
+
+    /* Send packet */
+    socket.send_to(b"Hejhej:)", "10.100.23.129:20022")?;
 
     Ok(())
 }
@@ -32,7 +41,7 @@ fn udp_test() -> Result<()> {
 fn tcp_test() -> Result<()> {
 
     /* Create socket */
-    let listener = TcpListener::bind("127.0.0.1:34254")?;
+    let listener = TcpListener::bind("0.0.0.0:34933")?;
 
     /* Accept a single connection */
     let (mut stream, addr) = listener.accept()?;
@@ -57,6 +66,39 @@ fn tcp_test() -> Result<()> {
 
     Ok(())
 }
+
+fn tcp_connect() -> Result<()> {
+
+    let mut send_buf = [0; 1024];
+
+    /* Create socket */
+    let listener = TcpListener::bind("10.100.23.32:20093")?;
+    let mut send_stream = TcpStream::connect("10.100.23.129:33546")?;
+
+    send_stream.write(b"Connect to: 10.100.23.32:20093\0")?;
+
+    send_stream.read(&mut send_buf)?;
+    println!("Received on 33546: \n\r{}", std::str::from_utf8(&send_buf).unwrap());
+
+    /* Accept a single connection */
+    let (mut recv_stream, addr) = listener.accept()?;
+    println!("Connection established with {}:\n\r", addr);
+
+    send_stream.write(b"U dog\0")?;
+    send_stream.write(b"Woof\0")?;
+    send_stream.write(b"Woof\0")?;
+
+    /* Buffer to store the received data */
+    let mut recv_buf = [0; 1024];
+
+    /* Read data from the stream */
+    let amt = recv_stream.read(&mut recv_buf)?;
+    let received_data = std::str::from_utf8(&recv_buf[..amt])
+        .expect("Could not write buffer as string");
+    println!("Received data on : {}:\n\r", received_data);
+
+    Ok(())
+} 
 
 /* Commands for testing on MacOS using netcat */
 
